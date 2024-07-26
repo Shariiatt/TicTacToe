951f5c8d0cb511ba084a1375be4e7339b228114d
shaffan <shaffan@duality.company>
2024-07-26 10:39:11 +0500

    second commit

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ var player2 = document.getElementById("player2");
 var winnerMessage = document.getElementById("winnerMessage");
 var form = document.getElementById("form");
 var gamebutton = document.getElementById("gamebutton");
+var Player1turn = document.getElementById("Player1turn");
+var Player2turn = document.getElementById("Player2turn");
 
-
-
-gamebutton.addEventListener("click" , () => {
+gamebutton.addEventListener("click" , (e) => {
     window.location.href = "game.html";
 })
+
+
+
+
+   
